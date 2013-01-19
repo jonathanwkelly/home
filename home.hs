132bed619797ae673bac0eb03879d3969a284894
Jonas Westerlund <jonas.westerlund@me.com>
2013-01-19 23:57:44 +0100

    Rewrite for Hakyll 4.0

--- home.hs
+++ home.hs
@@ -1,63 +1,102 @@
 {-# LANGUAGE ExplicitForAll, OverloadedStrings, UnicodeSyntax #-}
 
-import Control.Arrow (arr)
-import Control.Arrow.Unicode ((⁂), (⋙))
-import Control.Category (id)
-import Control.Monad (forM_)
-import Control.Monad.Unicode ((≫))
+import Control.Applicative ((<$>))
+import Control.Monad.Unicode ((≫), (≫=))
 import Data.Char (isLetter)
 import Data.List.Unicode ((⧺))
-import Data.Monoid (mconcat)
+import Data.Monoid (mappend, mconcat)
 import Data.Monoid.Unicode ((∅))
 import Hakyll
-import Prelude (($), IO, String, const, dropWhile, flip, map, not, reverse, take)
+import Prelude (($), IO, String, const, dropWhile, flip, fmap, map, not, return, reverse, take)
 import Prelude.Unicode ((∘))
 import System.FilePath (takeFileName)
 
 main ∷ IO ()
-main = hakyll $ do
-  match "index.html" $ route idRoute
-  match "posts.html" $ route idRoute
-  match "rss.xml"    $ route idRoute
-  match "tags/*"     ∘ route $ setExtension "html"
+main = hakyllWith config $ do
+  match ("fonts/**" .||. "img/**" .||. "js/**" .||. "files/**") $
+    route idRoute ≫ compile copyFileCompiler
 
-  _ ← match "posts/*" $ do
-    route   $ setExtension "html" `composeRoutes` postRoute
-    compile $ pageCompiler ⋙ postCompiler
+  match "css/**" $ route idRoute ≫ compile compressCssCompiler
 
-  _ ← create "index.html" $ constA (∅)
-    ⋙ arr (setField "title" "Index")
-    ⋙ requireAllA "posts/*" (id ⁂ arr (take 10 ∘ reverse ∘ chronological) ⋙ postList)
-    ⋙ applyTemplateCompiler "pages/index.html"
-    ⋙ applyTemplateCompiler "templates/default.html"
+  tags ← buildTags "posts/*" (fromCapture "tag/*.html")
 
-  _ ← create "posts.html" $ constA (∅)
-    ⋙ arr (setField "title" "All posts")
-    ⋙ requireAllA "posts/*" postList
-    ⋙ applyTemplateCompiler "templates/postlist.html"
-    ⋙ applyTemplateCompiler "templates/default.html"
+  match "posts/*" $ do
+    route   $ setExtension ".html" `composeRoutes` postRoute
+    compile $ pandocCompiler
+      ≫= saveSnapshot "content"
+      ≫= return ∘ fmap demoteHeaders
+      ≫= loadAndApplyTemplate "templates/postmeta.html" (postContext tags)
+      ≫= loadAndApplyTemplate "templates/post.html" (postContext tags)
+      ≫= loadAndApplyTemplate "templates/default.html" (postContext tags)
+      ≫= relativizeUrls
 
-  _ ← create "tags" $ requireAll "posts/*" (const readTags ∷ t → [Page String] → Tags String)
+  create ["posts.html"] $ do
+    route idRoute
+    compile $ do
+      list ← postList tags "posts/*" recentFirst
+      makeItem ""
+        ≫= loadAndApplyTemplate "templates/postlist.html"
+          (constField "title" "Posts" `mappend`
+            constField "posts" list `mappend`
+            defaultContext)
+        ≫= loadAndApplyTemplate "templates/default.html" defaultContext
+        ≫= relativizeUrls
 
-  metaCompile $ require_ "tags"
-    ⋙ arr tagsMap
-    ⋙ arr (map makeList)
+  -- Post tags
+  tagsRules tags $ \ tag pattern → do
+    let title = "Posts tagged with " ⧺ tag
+    route idRoute
+    compile $ do
+      list ← postList tags pattern recentFirst
+      makeItem ""
+        ≫= loadAndApplyTemplate "templates/postlist.html"
+            (constField "title" title `mappend`
+              constField "posts" list `mappend`
+              defaultContext)
+        ≫= loadAndApplyTemplate "templates/default.html" defaultContext
+        ≫= relativizeUrls
 
-  forM_ ["templates/*", "pages/*"] ∘ flip match $ compile templateCompiler
+  create ["index.html"] $ do
+    route topRoute
+    compile $ do
+      list ← postList tags "posts/*" $ take 10 ∘ recentFirst
+      makeItem list
+      let indexContext = constField "posts" list `mappend` defaultContext
+      makeItem ""
+        ≫= loadAndApplyTemplate "pages/index.html" indexContext
+        ≫= loadAndApplyTemplate "templates/default.html" indexContext
+        ≫= relativizeUrls
 
-  _ ← create "rss.xml" $ requireAll_ "posts/*" ⋙ renderRss feedConfiguration
-  _ ← match "css/**" $ route idRoute ≫ compile compressCssCompiler
-  forM_ ["fonts/**", "img/**", "js/**", "files/**"] ∘ flip match $ route idRoute ≫ compile copyFileCompiler
+  create ["rss.xml"] $ do
+    route idRoute
+    compile $ do
+      posts ← recentFirst <$> loadAllSnapshots "posts/*" "content"
+      renderRss feedConfiguration feedContext posts
 
-makeList ∷ ∀ α β. (String, [Page String]) → (Identifier α, Compiler β (Page String))
-makeList (t, p) = (tagIdentifier t, makeTagList t p)
+  match "templates/*" ∘ compile $ templateCompiler
+  match "pages/*" ∘ compile $ templateCompiler
 
-postList ∷ ∀ α. Compiler (Page α, [Page String]) (Page α)
-postList = setFieldA "posts" $
-  arr (reverse ∘ chronological)
-  ⋙ require "templates/postitem.html" (flip (map ∘ applyTemplate))
-  ⋙ arr mconcat
-  ⋙ arr pageBody
+postList ∷ Tags → Pattern → ([Item String] → [Item String]) → Compiler String
+postList tags pattern preprocess' = do
+    postItemTpl ← loadBody "templates/postitem.html"
+    posts       ← preprocess' <$> loadAll pattern
+    applyTemplateList postItemTpl (postContext tags) posts
+
+postContext ∷ Tags → Context String
+postContext tags = mconcat
+  [ modificationTimeField "mtime" "%U"
+  , dateField "date" "%B %e, %Y"
+  , dateField "isoDate" "%Y-%m-%d"
+  , tagsField "prettytags" tags
+  , defaultContext
+  ]
+
+feedContext ∷ Context String
+feedContext = mconcat
+  [ bodyField "description"
+  , dateField "date" "%B %e, %Y"
+  , defaultContext
+  ]
 
 -- Turn "posts/2011-11-21-post.html" into "post.html"
 postRoute ∷ Routes
@@ -67,23 +106,11 @@ postRoute = customRoute $ dropWhile (not ∘ isLetter) ∘ takeFileName ∘ toFi
 topRoute ∷ Routes
 topRoute = customRoute (takeFileName ∘ toFilePath)
 
-postCompiler ∷ Compiler (Page String) (Page String)
-postCompiler = arr (copyField "date" "isoDate")
-  ⋙ arr (renderDateField "date" "%B %e, %Y" "Date unknown")
-  ⋙ arr (renderDateField "isoDate" "%Y-%m-%d" "Date unknown")
-  ⋙ renderTagsField "prettytags" (fromCapture "tags/*")
-  ⋙ applyTemplateCompiler "templates/postmeta.html"
-  ⋙ applyTemplateCompiler "templates/post.html"
-  ⋙ applyTemplateCompiler "templates/default.html"
-
-makeTagList ∷ ∀ α. String → [Page String] → Compiler α (Page String)
-makeTagList tag posts = constA ((∅), posts)
-  ⋙ postList ⋙ arr (setField "title" ("Posts tagged with " ⧺ tag))
-  ⋙ applyTemplateCompiler "templates/postlist.html"
-  ⋙ applyTemplateCompiler "templates/default.html"
-
-tagIdentifier ∷ ∀ α. String → Identifier α
-tagIdentifier = fromCapture "tags/*"
+config ∷ Configuration
+config = defaultConfiguration
+  { deployCommand = "rsync --checksum -avz \
+                    \_site/* jonas@192.168.11.1:/usr/local/www/jonas/jonaswesterlund.se/"
+  }
 
 feedConfiguration ∷ FeedConfiguration
 feedConfiguration = FeedConfiguration
