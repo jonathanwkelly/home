b821aeff10a8500398204b0786934975b8a1c3f5
Jonas Westerlund <jonas.westerlund@me.com>
2013-07-21 23:47:44 +0200

    Remove unicode stuff
    Update CSS
    
    Update index page

--- home.hs
+++ home.hs
@@ -1,81 +1,76 @@
-{-# LANGUAGE ExplicitForAll, OverloadedStrings, UnicodeSyntax #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad (forM_)
-import Control.Monad.Unicode ((≫), (≫=), (=≪))
 import Data.Char (isLetter)
-import Data.List.Unicode ((⧺))
-import Data.Monoid (mconcat)
-import Data.Monoid.Unicode ((⊕))
+import Data.Monoid ((<>), mconcat)
 import Hakyll
-import Prelude.Unicode ((∘))
 import System.FilePath (takeFileName)
 
-main ∷ IO ()
 main = hakyllWith config $ do
   match ("fonts/**" .||. "img/**" .||. "js/**" .||. "files/**") $
-    route idRoute ≫ compile copyFileCompiler
+    route idRoute >> compile copyFileCompiler
 
-  match "css/**" $ route idRoute ≫ compile compressCssCompiler
+  match "css/**" $ route idRoute >> compile compressCssCompiler
 
-  tags ← buildTags "posts/*" $ fromCapture "tag/*.html"
+  tags <- buildTags "posts/*" $ fromCapture "tag/*.html"
 
   match "posts/*" $ do
     route   $ setExtension ".html" `composeRoutes` postRoute
     compile $ pandocCompiler
-      ≫= saveSnapshot "content"
-      ≫= loadAndApplyTemplate "templates/postmeta.html" (postContext tags)
-      ≫= loadAndApplyTemplate "templates/post.html" (postContext tags)
-      ≫= loadAndApplyTemplate "templates/default.html" (postContext tags)
-      ≫= relativizeUrls
+      >>= saveSnapshot "content"
+      >>= loadAndApplyTemplate "templates/postmeta.html" (postContext tags)
+      >>= loadAndApplyTemplate "templates/post.html" (postContext tags)
+      >>= loadAndApplyTemplate "templates/default.html" (postContext tags)
+      >>= relativizeUrls
 
   create ["posts.html"] $ do
     route idRoute
     compile $ do
-      list ← postList tags "posts/*"
+      list <- postList tags "posts/*"
+      let postListContext = constField "title" "All posts" <> constField "posts" list <> defaultContext
       makeItem ""
-        ≫= loadAndApplyTemplate "templates/postlist.html"
-          (constField "title" "Posts" ⊕ constField "posts" list ⊕ defaultContext)
-        ≫= loadAndApplyTemplate "templates/default.html" defaultContext
-        ≫= relativizeUrls
+        >>= loadAndApplyTemplate "templates/postlist.html" postListContext
+        >>= loadAndApplyTemplate "templates/default.html"  postListContext
+        >>= relativizeUrls
 
   -- Post tags
-  tagsRules tags $ \ tag pattern → do
-    let title = "Posts tagged with " ⧺ tag
+  tagsRules tags $ \ tag pattern -> do
+    let title = "Posts tagged with " <> tag
     route idRoute
     compile $ do
-      list ← postList tags pattern
+      list <- postList tags pattern
+      let tagListContext = constField "title" title <> constField "posts" list <> defaultContext
       makeItem ""
-        ≫= loadAndApplyTemplate "templates/postlist.html"
-            (constField "title" title ⊕ constField "posts" list ⊕ defaultContext)
-        ≫= loadAndApplyTemplate "templates/default.html" defaultContext
-        ≫= relativizeUrls
+        >>= loadAndApplyTemplate "templates/postlist.html" tagListContext
+        >>= loadAndApplyTemplate "templates/default.html"  tagListContext
+        >>= relativizeUrls
 
   create ["index.html"] $ do
     route topRoute
     compile $ do
-      list ← postList tags "posts/*"
+      list <- postList tags "posts/*"
       makeItem list
-      let indexContext = constField "posts" list ⊕ defaultContext
+      let indexContext = constField "posts" list <> constField "title" "Index" <> defaultContext
       makeItem ""
-        ≫= loadAndApplyTemplate "pages/index.html" indexContext
-        ≫= loadAndApplyTemplate "templates/default.html" indexContext
-        ≫= relativizeUrls
+        >>= loadAndApplyTemplate "pages/index.html" indexContext
+        >>= loadAndApplyTemplate "templates/default.html" indexContext
+        >>= relativizeUrls
 
   create ["rss.xml"] $ do
     route idRoute
     compile $ do
-      posts ← recentFirst =≪ loadAllSnapshots "posts/*" "content"
+      posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
       renderRss feedConfiguration feedContext posts
 
   forM_ ["templates/*", "pages/*"] $ flip match $ compile templateCompiler
 
-postList ∷ Tags → Pattern → Compiler String
+postList :: Tags -> Pattern -> Compiler String
 postList tags pattern = do
-    postItemTpl ← loadBody "templates/postitem.html"
-    posts       ← recentFirst =≪ loadAll pattern
+    postItemTpl <- loadBody "templates/postitem.html"
+    posts       <- recentFirst =<< loadAll pattern
     applyTemplateList postItemTpl (postContext tags) posts
 
-postContext ∷ Tags → Context String
+postContext :: Tags -> Context String
 postContext tags = mconcat
   [ modificationTimeField "mtime" "%U"
   , dateField "date" "%B %e, %Y"
@@ -84,7 +79,6 @@ postContext tags = mconcat
   , defaultContext
   ]
 
-feedContext ∷ Context String
 feedContext = mconcat
   [ bodyField "description"
   , dateField "date" "%B %e, %Y"
@@ -92,20 +86,16 @@ feedContext = mconcat
   ]
 
 -- Turn "posts/2011-11-21-post.html" into "post.html"
-postRoute ∷ Routes
-postRoute = customRoute $ dropWhile (not ∘ isLetter) ∘ takeFileName ∘ toFilePath
+postRoute = customRoute $ dropWhile (not . isLetter) . takeFileName . toFilePath
 
 -- Turn "pages/foo.html" into "foo.html"
-topRoute ∷ Routes
-topRoute = customRoute (takeFileName ∘ toFilePath)
+topRoute = customRoute (takeFileName . toFilePath)
 
-config ∷ Configuration
 config = defaultConfiguration
   { deployCommand = "rsync --checksum -avz \
                     \_site/* jonas@192.168.11.1:/usr/local/www/jonas/jonaswesterlund.se/"
   }
 
-feedConfiguration ∷ FeedConfiguration
 feedConfiguration = FeedConfiguration
   { feedTitle       = "Jonas Westerlund’s blog"
   , feedDescription = "Personal blog of Jonas Westerlund"
